cf24999cfdaf91a42dd1a00dd90cd1255ff06e04
dashcom <dashcom@protonmail.com>
2018-03-15 04:48:42 +0100

    gestion de noscript js separe

---
+++ site/assets/js/player.js
@@ -0,0 +1,6 @@
+document.getElementById('player').outerHTML = `
+<video data-dashjs-player width="698px"
+src="http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/ElephantsDream/1sec/ElephantsDream_1s_onDemand_2014_05_09.mpd"
+controls poster="assets/images/banner.png">
+<p>Ça marche pas :(</br>Reportez vous sur les autres options de stream citées ci dessous</p>
+</video>`;
